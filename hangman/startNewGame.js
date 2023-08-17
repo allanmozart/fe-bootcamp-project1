2519109278563db0bcd5eed81b2a1e9736702e16
ca-trindade <piratum.core@gmail.com>
2023-08-17 17:14:45 +0100

    start new game menu

---
+++ hangman/startNewGame.js
@@ -0,0 +1,62 @@
+
+
+
+function startNewGame() {
+
+
+    select({
+        message: "Select a game type",
+        choices: [
+            {
+                name: "Player vs Player",
+                value: select({
+                    message: "difficulty level",
+                    choices: [
+                        {
+                            name: "Easy",
+                            value: "Easy",
+                        },
+                        {
+                            name: "Medium",
+                            value: "Medium",
+                        },
+                        {
+                            name: "Hard",
+                            value: "Hard",
+                        }
+                    ],
+                }).then(function (x) {
+                    //resultado da escolha Player vs Player - Guardar para load Game
+
+                })
+            },
+            {
+                name: "Player vs Computer",
+                value: select({
+                    message: "difficulty level",
+                    choices: [
+                        {
+                            name: "Easy",
+                            value: "Easy",
+                        },
+                        {
+                            name: "Medium",
+                            value: "Medium",
+                        },
+                        {
+                            name: "Hard",
+                            value: "Hard",
+                        }
+                    ],
+                }).then(function (x) {
+                    //resultado da escolha Player vs Computer - Guardar para load Game
+
+                })
+            }
+        ],
+    }).then(function (x) {
+        //resultado da escola de select() - Guardar para load Game
+
+    });
+
+}
