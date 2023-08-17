6d855c08d26d302035f3dcef6c78f0beb71aeee4
Emanuel Leal <emanuel.s.leal@gmail.com>
2023-08-17 16:51:45 +0100

    adding the new game sub menu

--- bin/index.js
+++ bin/index.js
@@ -7,12 +7,12 @@ console.log("main index");
 //Main menu
 //start new game
 //Select game type
-//Player vs Player
-//Select difficulty level
-//Levels (ex: easy, medium and high)
-//Player vs Computer
-//Select difficulty level
-//Levels (ex: easy, medium and high)
+    //Player vs Player
+        //Select difficulty level
+            //Levels (ex: easy, medium and high)
+    //Player vs Computer
+        //Select difficulty level
+        //Levels (ex: easy, medium and high)
 //Save game
 //Load game
 //exit
@@ -21,12 +21,42 @@ function mainMenu() {
   rawlist({
     message: "The Hangman game!!!!",
     choices: [
-      { name: "Start new game", value: "newGame" },
+      { name: "Start new game", value: "new" },
       { name: "Save game", value: "save" },
       { name: "Load game", value: "load" },
       { name: "Exit", value: "exit" },
     ],
-  });
+  }).then(function(option){
+    switch(option){
+        case "new":
+            startGame();
+            break;
+        case "save":
+            saveGame();
+            break;
+        case "load":
+            loadGame();
+            break;
+        default:
+            console.log("Select a valid option");
+    }
+  })
 }
 
 mainMenu();
+
+function startGame(){
+    select({
+        message: "Select the game type",
+        choices: [
+          {
+            name: "Player Vs Player",
+            value: "player",
+          },
+          {
+            name: "Player Vs PC",
+            value: "playerpc",
+          },
+        ],
+      }) 
+}
