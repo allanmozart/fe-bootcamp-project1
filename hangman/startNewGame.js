6cc09601673265745b23957351f998f4ac1d8402
ca-trindade <piratum.core@gmail.com>
2023-08-17 19:54:46 +0100

    organizacion folders

--- hangman/startNewGame.js
+++ hangman/startNewGame.js
@@ -1,62 +1,58 @@
-
-
+const { input, rawlist, select } = require("@inquirer/prompts");
 
 function startNewGame() {
-
-
-    select({
-        message: "Select a game type",
-        choices: [
+  select({
+    message: "Select a game type",
+    choices: [
+      {
+        name: "Player vs Player",
+        value: select({
+          message: "difficulty level",
+          choices: [
             {
-                name: "Player vs Player",
-                value: select({
-                    message: "difficulty level",
-                    choices: [
-                        {
-                            name: "Easy",
-                            value: "Easy",
-                        },
-                        {
-                            name: "Medium",
-                            value: "Medium",
-                        },
-                        {
-                            name: "Hard",
-                            value: "Hard",
-                        }
-                    ],
-                }).then(function (x) {
-                    //resultado da escolha Player vs Player - Guardar para load Game
-
-                })
+              name: "Easy",
+              value: "Easy",
             },
             {
-                name: "Player vs Computer",
-                value: select({
-                    message: "difficulty level",
-                    choices: [
-                        {
-                            name: "Easy",
-                            value: "Easy",
-                        },
-                        {
-                            name: "Medium",
-                            value: "Medium",
-                        },
-                        {
-                            name: "Hard",
-                            value: "Hard",
-                        }
-                    ],
-                }).then(function (x) {
-                    //resultado da escolha Player vs Computer - Guardar para load Game
-
-                })
-            }
-        ],
-    }).then(function (x) {
-        //resultado da escola de select() - Guardar para load Game
-
-    });
-
+              name: "Medium",
+              value: "Medium",
+            },
+            {
+              name: "Hard",
+              value: "Hard",
+            },
+          ],
+        }).then(function (x) {
+          //resultado da escolha Player vs Player - Guardar para load Game
+        }),
+      },
+      {
+        name: "Player vs Computer",
+        value: select({
+          message: "difficulty level",
+          choices: [
+            {
+              name: "Easy",
+              value: "Easy",
+            },
+            {
+              name: "Medium",
+              value: "Medium",
+            },
+            {
+              name: "Hard",
+              value: "Hard",
+            },
+          ],
+        }).then(function (x) {
+          //resultado da escolha Player vs Computer - Guardar para load Game
+        }),
+      },
+    ],
+  }).then(function (x) {
+    //resultado da escola de select() - Guardar para load Game
+  });
 }
+
+startNewGame();
+module.export = { startNewGame };
